5264452d7ec7738bb2c40f986ce0779ca23ca270
mosePark <58103517+mosePark@users.noreply.github.com>
2025-02-03 16:16:24 +0900

    Update tutorial.sql

--- mode/tutorial.sql
+++ mode/tutorial.sql
@@ -1,3 +1,4 @@
+-- SQL SELECT
 -- SELECT *
 --   FROM tutorial.us_housing_units
   
@@ -6,3 +7,25 @@
 --       south
 --   FROM tutorial.us_housing_units
 
+-- SELECT west AS "West Region"
+--   FROM tutorial.us_housing_units
+
+-- SELECT west AS West_Region,
+--       south AS South_Region
+--   FROM tutorial.us_housing_units;
+------------------------------------------------
+-- 공백과 따옴표의 중요성
+-- SELECT midwest as "Midwest",
+--         month as "Month",
+--         month_name as "Month_name",
+--         northeast as "Northeast",
+--         south as "South",
+--         west as "West",
+--         year as "Year"
+--   FROM tutorial.us_housing_units
+------------------------------------------------
+-- SQL LIMIT
+
+-- SELECT *
+--   FROM tutorial.us_housing_units
+--   LIMIT 10
